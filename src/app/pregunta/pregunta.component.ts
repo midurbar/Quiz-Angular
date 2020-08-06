1e065ea0e67b7e9076d60ed2ecd3bccdd814cd84
afd <afd@alup5.academiapostal.es.local>
2020-08-06 20:53:25 +0200

    canbios en componentes pregunta y respuesta

--- src/app/pregunta/pregunta.component.ts
+++ src/app/pregunta/pregunta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pregunta',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pregunta.component.css']
 })
 export class PreguntaComponent implements OnInit {
+
+  @Input()
+  titulo : string = "";
 
   constructor() { }
 
