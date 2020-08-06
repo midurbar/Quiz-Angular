4cadc577300d8b09c9554b280355bb4939d1278b
afd <afd@alup5.academiapostal.es.local>
2020-08-06 20:20:43 +0200

    creado componentes pregunta, respuesta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PreguntaComponent } from './pregunta/pregunta.component';
+import { RespuestaComponent } from './respuesta/respuesta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreguntaComponent,
+    RespuestaComponent
   ],
   imports: [
     BrowserModule
