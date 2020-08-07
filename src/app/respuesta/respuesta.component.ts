32b5b685560d39dacb7c941a3810578f81b5a307
afd <afd@alup5.academiapostal.es.local>
2020-08-07 18:56:59 +0200

    cambio respuestas

--- src/app/respuesta/respuesta.component.ts
+++ src/app/respuesta/respuesta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-respuesta',
@@ -16,13 +16,16 @@ export class RespuestaComponent implements OnInit {
   @Input()
   color : string;
 
+  @Output()
+  seleccion = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   seleccionar() {
-    alert("Opcion "+ this.opcion)
+    this.seleccion.emit(this.opcion);
   }
 
 }
