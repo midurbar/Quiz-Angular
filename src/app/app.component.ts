de06daa4039ef159f026832c69226f0166b333ea
afd <afd@alup5.academiapostal.es.local>
2020-08-07 17:16:29 +0200

    añadido preguntas respuestas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  /**
+   * Esta variable contendra la pregunta que se estara mostrando por
+   * pantalla
+   */
+  preguntaActual = undefined;
+
+  /**
+   * En el momento de cargar el componente, se elegira una pregunta
+   * a mostrar de entre todas los disponibles en preguntas.
+   */
+  ngOnInit(): void {
+    this.preguntaActual = this.preguntas[0];
+  }
+
   title = 'quiz';
 
   preguntas = [
@@ -15,10 +30,20 @@ export class AppComponent {
         "Blanco",
         "Negro",
         "Rojo",
-        "Azul"
+        "Isabelino"
       ],
       correcta: 3
     },
-    
-  ]
+    {
+      pregunta: "En que se año se celebraron los juegos olimpicos de tokio 2020",
+      respuestas: [
+        "2020",
+        "2021",
+        "2019",
+        "Ninguna de las anteriores"
+      ],
+      correcta: 1
+    },
+  ];
+
 }
